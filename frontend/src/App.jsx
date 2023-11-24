98d6b8691e41f9b5c657d2a54f3a870f58083a64
afsaints0987 <saints0987@gmail.com>
2023-11-24 20:17:16 +0800

    fixed re-render of creating user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,26 @@
 import { SendBirdProvider } from "@sendbird/uikit-react";
 import ChatApp from "./components/ChatApp";
 import "@sendbird/uikit-react/dist/index.css";
-// import { useCreateUser } from "./hooks/useCreateUser";
+import { useCreateUser } from "./hooks/useCreateUser";
 
 
 const APP_ID = import.meta.env.VITE_APP_ID;
-const USER_ID = "730cf02b-989a-52ad-9639-a31e8506bfbb";
+// const USER_ID = "730cf02b-989a-52ad-9639-a31e8506bfbb";
 
 function App() {
-  // const { userData, loading } = useCreateUser();
+  const { userData, loading } = useCreateUser();
   
-  // const { nickname, profileUrl, userId } = userData
-  // console.log(nickname, profileUrl, userId)
+  const { nickname, profileUrl, userId } = userData
+  console.log(nickname, profileUrl, userId)
 
   return (
     <div className="App">
       {/* {loading && console.log('Loading...')} */}
       <SendBirdProvider
         appId={APP_ID}
-        userId={USER_ID}
-        // nickname={nickname}
-        // profileUrl={profileUrl}
+        userId={userId}
+        nickname={nickname}
+        profileUrl={profileUrl}
       >
         <ChatApp />
       </SendBirdProvider>
--- frontend/src/hooks/useCreateUser.js
+++ frontend/src/hooks/useCreateUser.js
@@ -1,52 +1,51 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { http } from "../config/axios";
 
 export const useCreateUser = () => {
   const [userData, setUserData] = useState({
     userId: "",
     nickname: "",
-    profileUrl: ""
-  })
-  const [loading, setLoading] = useState(false)
+    profileUrl: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchAndSaveUser = useCallback(async () => {
+    try {
+      setLoading(true);
+      const response = await http.get(`${import.meta.env.VITE_USER_URL}`);
+      const userDataResponse = await response.data;
+
+      const { login, name, picture } = userDataResponse.results[0];
+      const userId = login.uuid;
+      const nickname = name.first;
+      const profileUrl = picture.large;
+
+      await http.post("/create-user", {
+        user_identifier: userId,
+        nickname,
+        profile_url: profileUrl,
+      });
+
+      console.log("User created successfully!");
+      alert("New User Created")
+
+      setUserData({
+        userId,
+        nickname,
+        profileUrl,
+      });
+    } catch (error) {
+      console.error("Error creating user", error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []); // No dependencies, so the function won't be recreated
 
   useEffect(() => {
-    setLoading(true)
-    const fetchAndSaveUser = async () => {
-      try {
-        // Fetch user data
-        const response = await fetch(`${import.meta.env.VITE_USER_URL}`);
-        const userDataResponse = await response.json();
-
-        // Extract relevant user information
-        const { login, name, picture } = userDataResponse.results[0];
-        const userId = login.uuid
-        const nickname = name.first;
-        const profileUrl = picture.large;
-
-        // Save user data to the backend
-        await http.post("/create-user", {
-          user_identifier: userId,
-          nickname,
-          profile_url: profileUrl
-        });
-
-        console.log("User created successfully!");
-
-        // Update the state with user data
-        setUserData({
-          userId,
-          nickname,
-          profileUrl,
-        });
-        setLoading(false)
-      } catch (error) {
-        console.error("Error creating user", error);
-      }
-    };
-
-    // Call the function to fetch and save user data
     fetchAndSaveUser();
-  }, []); // The empty dependency array ensures this effect runs only once on mount
+  }, [fetchAndSaveUser]);
 
-  return {userData, loading};
+  return { userData, loading, error };
 };
